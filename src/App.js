43caf6d8f715f1d11bbcacd6af66384bc9b8611b
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-03-12 19:28:55 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from "react";
-import DoughnutChart from "./components/DoughnutChart";
-import './App.css';
 
-function App() {
-  return (
-    <div className="app-container">
-      <h1>Top Artists </h1>
-      <DoughnutChart />
-    </div>
-  );
-}
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import DoughnutChart from "./components/DoughnutChart";
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="app-container">
+//       <h1>Top Artists </h1>
+//       <DoughnutChart />
+//     </div>
+//   );
+// }
+
+// export default App;
