e788cd3f5445206d2a6b0e60ad79b7e09095d2ec
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-03-12 19:56:26 +0200

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DoughnutChart from "./components/DoughnutChart";
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Top Artists </h1>
+      <DoughnutChart />
+    </div>
+  );
+}
+
+export default App;
